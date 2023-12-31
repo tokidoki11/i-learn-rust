d140fc0785ecf93c3450f60056856906ec9c433d
Han Sen <sen.durandal@gmail.com>
2023-12-31 19:15:06 +0900

    deref smar pinter + iteration

---
+++ borrowing/src/main.rs
@@ -0,0 +1,25 @@
+//talking about deref trait in smart pointer
+
+use std::ops::Deref;
+struct MyBox<T>(T);
+
+impl<T> MyBox<T> {
+    fn new(x: T) -> MyBox<T> {
+        MyBox(x)
+    }
+}
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+fn main() {
+    let x = 5;
+    let y = MyBox::new(x); // x is taken ownership by the func but actually copied (native has copy trait)
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+}
