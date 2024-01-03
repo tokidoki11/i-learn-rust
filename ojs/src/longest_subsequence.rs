03ee77810a966337a5b18504656c1098b8908aa3
Han Sen <sen.durandal@gmail.com>
2024-01-03 10:25:43 +0900

    use as bytes instead

--- ojs/src/longest_subsequence.rs
+++ ojs/src/longest_subsequence.rs
@@ -6,10 +6,12 @@ pub struct Solution {}
 // tle
 impl Solution {
     pub fn longest_common_subsequence(text1: String, text2: String) -> i32 {
-        let mut memo = vec![vec![0; text2.len() + 1]; text1.len() + 1];
-        let m = text1.len() as i32;
-        let n = text2.len() as i32;
-        Self::lcs(&text1, &text2, m - 1, n - 1, &mut memo)
+        // let mut memo = vec![vec![0; text2.len() + 1]; text1.len() + 1];
+        // let m = text1.len() as i32;
+        // let n = text2.len() as i32;
+        // Self::lcs(&text1, &text2, m - 1, n - 1, &mut memo)
+
+        Self::buttom_up(text1, text2)
     }
 
     fn lcs(X: &str, Y: &str, m: i32, n: i32, memo: &mut Vec<Vec<i32>>) -> i32 {
@@ -28,5 +30,26 @@ impl Solution {
             );
         }
         memo[m as usize][n as usize]
+    }
+
+    // from front (plusing) (not allowed - middle table is supposedly blank)
+    fn buttom_up(text1: String, text2: String) -> i32 {
+        let mut memo = vec![vec![0; text2.len() + 1]; text1.len() + 1];
+        let m = text1.len();
+        let n = text2.len();
+
+        for i in 1..=m {
+            for j in 1..=n {
+                let c1 = text1.as_bytes()[i - 1];
+                let c2 = text2.as_bytes()[j - 1];
+                if c1 == c2 {
+                    memo[i][j] = 1 + memo[i - 1][j - 1]
+                } else {
+                    memo[i][j] = max(memo[i - 1][j], memo[i][j - 1])
+                }
+            }
+        }
+
+        memo[m][n]
     }
 }
